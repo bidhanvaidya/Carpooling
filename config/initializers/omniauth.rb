aa8feaee164e5482d147c0ef64f7285264857007
Bidhan Vaidya <test@test.com>
2013-01-16 19:37:56 +1100

    fcbk update

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
+OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '414998911867511', '29ad937c5e4f060398af36cde9e60660',:display => 'popup', :scope => 'user_work_history,email,user_education_history,user_interests,user_hometown,user_location,user_actions.music'
 end
