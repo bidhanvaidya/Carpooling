7d3e614b54673e2c25ef10e0e73e2136ee57a023
Bidhan Vaidya <test@test.com>
2013-01-16 19:32:56 +1100

    fcbk update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,8 @@ class SessionsController < ApplicationController
 def create
 	#raise request.env["omniauth.auth"].to_yaml
   auth = request.env["omniauth.auth"]
-  user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+  user = User.from_omniauth(env["omniauth.auth"])
+  #user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
    
 
   session[:user_id] = user.id
